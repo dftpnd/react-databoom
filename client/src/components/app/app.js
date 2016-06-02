9c5c9e0d6e60e22b2fabf452e6e2120f7ffa6726
Maksim Gryaznov <lkdnvc@gmail.com>
2016-06-02 20:21:46 +0300

    fix header

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -1,32 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router'
+import {Link} from 'react-router'
 
 
 //assets
 require('normalize.css/normalize.css');
 require('styles/index.scss');
+const imgPath = require('../../images/logotype.png');
 
 class App extends React.Component {
-	render() {
-		return (
-			<section className="app">
-				<header className="header">
-					<Link to='/' className="header__logo">
-						<p className="header__logo--grey">Auto</p>
-						<p>Gross</p>
-					</Link>
-					<nav className="menu">
-						<Link to='/auction' className="item" activeClassName="active">auction</Link>
-						<Link to='/add-car/car-form' className="item" activeClassName="active">add-car</Link>
-						<Link to='/login' className="item" activeClassName="active">login</Link>
-					</nav>
-				</header>
-				<div className="content-wrapper">
-					{this.props.children }
-				</div>
-			</section>
-		);
-	}
+  render() {
+    return (
+      <section className="app">
+        <header className="header">
+          <div className="limiter">
+            <nav className="header-menu">
+              <Link to='/' className="header__logo">
+                <p className="header__logo--grey">Auto</p>
+                <p>Gross</p>
+              </Link>
+              <Link to='/auction' className="item" activeClassName="active">auction</Link>
+              <Link to='/add-car/car-form' className="item" activeClassName="active">add-car</Link>
+            </nav>
+
+            <div className="header-auth">
+              <div className="header-auth__profile">login</div>
+              <div className="header-auth__logo">
+                <img src={imgPath} className="header-auth__logo"/>
+              </div>
+            </div>
+          </div>
+        </header>
+        <div className="content-wrapper limiter">
+          {this.props.children }
+        </div>
+      </section>
+    );
+  }
 }
 
 App.defaultProps = {};
