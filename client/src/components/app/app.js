8b5e76d8c9e2ac37f68ac7d3e348e746da22f448
lkdnvc <lkdnvc@gmail.com>
2016-06-14 22:22:28 +0300

    feat(add-car) add coment open modal

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -10,64 +10,61 @@ const imgPath = require('../../images/user.png');
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.logout = this.logout.bind(this);
-  }
+		this.logout = this.logout.bind(this);
+	}
 
-  logout()
-  {
-    localStorage.clear();
-    location.reload();
-  }
+	logout() {
+		localStorage.clear();
+		location.reload();
+	}
 
-  render() {
+	render() {
 
-    var res;
-    var userinfo = localStorage['userinfo'];
-    if(userinfo)
-    {
-      res = (<section className="app">
-        <header className="header">
-          <div className="limiter">
-            <nav className="header-menu">
-              <Link to='/' className="header__logo">
-                <p className="header__logo--grey">Auto</p>
-                <p>Gross</p>
-              </Link>
-              <Link to='/auction' className="item" activeClassName="active">auction</Link>
-              <Link to='/add-car' className="item" activeClassName="active">add-car</Link>
-              <Link to='/manager' className="item" activeClassName="active">manager</Link>
-              <Link to='/manager-sold' className="item" activeClassName="active">manager-sold</Link>
-              <Link to='/personal' className="item" activeClassName="active">personal</Link>
-            </nav>
+		var res;
+		var userinfo = localStorage['userinfo'];
+		if (userinfo) {
+			res = (<section className="app">
+				<header className="header">
+					<div className="limiter">
+						<nav className="header-menu">
+							<Link to='/' className="header__logo">
+								<p className="header__logo--grey">Auto</p>
+								<p>Gross</p>
+							</Link>
+							<Link to='/auction' className="item" activeClassName="active">auction</Link>
+							<Link to='/add-car' className="item" activeClassName="active">add-car</Link>
+							<Link to='/manager' className="item" activeClassName="active">manager</Link>
+							<Link to='/manager-sold' className="item" activeClassName="active">manager-sold</Link>
+							<Link to='/personal' className="item" activeClassName="active">personal</Link>
+						</nav>
 
-            <div className="header-auth">
-              <div className="header-auth__profile">
-                <a href="javascript:void(0)" onClick={this.logout}> Выход</a>
-              </div>
+						<div className="header-auth">
+							<div className="header-auth__profile">
+								<a href="javascript:void(0)" onClick={this.logout}> Выход</a>
+							</div>
 
-              <div className="header-auth__logo">
-                <img src={imgPath} className="header-auth__logo"/>
-              </div>
-              {auth.getUserTitle()}
-            </div>
-          </div>
-        </header>
+							<div className="header-auth__logo">
+								<img src={imgPath} className="header-auth__logo"/>
+							</div>
+							{auth.getUserTitle()}
+						</div>
+					</div>
+				</header>
 
-        {this.props.children }
+				{this.props.children }
 
-      </section>      )
-    }else
-    {
-      res = (<section className="app">
-        <Login/>
-      </section>      )
-    }
+			</section>      )
+		} else {
+			res = (<section className="app">
+				<Login/>
+			</section>      )
+		}
 
-    return res;
-  }
+		return res;
+	}
 }
 
 App.defaultProps = {};
--- client/src/components/group-button/group-button.js
+++ client/src/components/group-button/group-button.js
@@ -1,25 +1,39 @@
 import React from 'react';
-var classNames = require('classnames');
+import Modal from 'react-modal';
 
+var classNames = require('classnames');
+const addCommentSvg = require('../../images/add-comment.svg');
 class GroupButton extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			value: this.props.value
+			value: this.props.value,
+			modalIsOpen: false
 		};
 
 		this.clickHandlerOk = this.clickHandlerOk.bind(this);
 		this.clickHandlerNo = this.clickHandlerNo.bind(this);
+		this.openModal = this.openModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
 	}
 
 	clickHandlerOk() {
 		this.props.handler(this.props.name, true);
 		this.setState({value: true});
 	}
+
 	clickHandlerNo() {
 		this.props.handler(this.props.name, false);
 		this.setState({value: false});
+	}
+
+	openModal() {
+		this.setState({modalIsOpen: true});
+	}
+
+	closeModal() {
+		this.setState({modalIsOpen: false});
 	}
 
 	render() {
@@ -46,6 +60,19 @@ class GroupButton extends React.Component {
 				<button className={dangerButton} onClick={this.clickHandlerNo} type="button">
 					Нет
 				</button>
+				<button className="group-button__add-comment" onClick={this.openModal} title="Добавить комментарий">
+					<img src={addCommentSvg} alt="add coment" width="24" height="30"/>
+				</button>
+
+				<Modal
+					isOpen={this.state.modalIsOpen}
+					onRequestClose={this.publicateModalClose}
+					shouldCloseOnOverlayClick={false}>
+					<h2 className="modal__title">openModal</h2>
+
+					<button className="common-button put-on" onClick={this.publishCar}>Выставить на аукцион</button>
+					<button className="modal__close" onClick={this.publicateModalClose}></button>
+				</Modal>
 			</div>
 		);
 	}
